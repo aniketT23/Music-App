88b090399a56c8a8c204f65fba11870692fd2c73
Aniket <tiwarianiket91@gmail.com>
2022-01-18 13:26:37 +0530

    something

--- Backend/src/controllers/music.controllers.js
+++ Backend/src/controllers/music.controllers.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const router = express.Router();
 const artist = require("../models/artist.model");
 const List = require("../models/music.models");
-const { populate } = require("../models/music.models");
+const { populate, off } = require("../models/music.models");
 const list = require("../models/music.models");
 router.use(express.json());
 
@@ -14,8 +14,15 @@ router.post("/", async(req, res) => {
     return res.status(201).send({ List });
 });
 router.get("/", async(req, res) => {
-    const List = await list.find().lean().exec();
-    return res.status(200).send({ List });
+    const page = +req.query.page || 1;
+    const size = +req.query.size || 8;
+
+    const offset = (page - 1) * size;
+
+    const List = await list.find().skip(offset).limit(size).lean().exec();
+    const totalCount = await list.find().countDocuments().lean().exec();
+    const totalPages = Math.ceil(totalCount / size);
+    return res.status(200).send({ List, totalPages });
 });
 router.get("/search", async(req, res) => {
     let x = req.query.artist;
@@ -23,6 +30,65 @@ router.get("/search", async(req, res) => {
     const List = await list.find({ artist: x }).lean().exec();
 
     return res.status(200).send({ List });
+});
+
+router.get("/filter", async(req, res) => {
+    let x = req.query.gener;
+    let y = req.query.year;
+    const page = +req.query.page || 1;
+    const size = +req.query.size || 10;
+    console.log(size);
+    // console.log(x);
+    const offset = (page - 1) * size;
+
+    if (x && !y) {
+        const List = await list
+            .find({ gener: x })
+            .skip(offset)
+            .limit(size)
+            .lean()
+            .exec();
+        const totalCount = await list
+            .find({ gener: x })
+            .countDocuments()
+            .lean()
+            .exec();
+
+        const totalPages = Math.ceil(totalCount / size);
+        return res.status(200).send({ List, totalPages });
+    }
+    if (y && !x) {
+        const List = await list
+            .find({ year: y })
+            .skip(offset)
+            .limit(size)
+            .lean()
+            .exec();
+
+        const totalCount = await list
+            .find({ year: y })
+            .countDocuments()
+            .lean()
+            .exec();
+        // console.log(totalCount);
+        const totalPages = Math.ceil(totalCount / size);
+        // console.log(totalPages);
+        return res.status(200).send({ List, totalPages });
+    }
+    const List = await list
+        .find({ $and: [{ gener: x }, { year: y }] })
+        .skip(offset)
+        .limit(size)
+        .lean()
+        .exec();
+
+    const totalCount = await list
+        .find({ $and: [{ gener: x }, { year: y }] })
+        .countDocuments()
+        .lean()
+        .exec();
+    const totalPages = Math.ceil(totalCount / size);
+    return res.status(200).send({ List, totalPages });
 });
 
 router.get("/:id", async(req, res) => {
--- Backend/src/models/music.models.js
+++ Backend/src/models/music.models.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const musicSchema = new mongoose.Schema({
     artist: { type: String, required: true },
     Number_songs: { type: Number, required: true },
+    gener: { type: String },
+    year: { type: Number },
     photo: { type: String, required: true },
 }, {
     versionKey: false,
